fe75d4aa2e2d9987de0920997710e47928e6416d
Rolthar <dominicmazur@hotmail.com>
2020-02-06 14:54:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ io.on('connection', function (socket) {
 
 
     try {
-      socket.join(data.currentRoom);
+      socket.leave(data.currentRoom);
       console.log(data.userID + ' left room : ' + data.currentRoom);
       io.sockets.in(data.currentRoom).emit('leave room', data.userID + ' left room : ' + data.currentRoom);
     }
